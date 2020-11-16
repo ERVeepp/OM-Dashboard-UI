9f6a63b26137e866bed4f320cd04814b87779391
王伟 <ERVeepp@users.noreply.github.com>
2020-11-16 16:23:53 +0800

    接口地址
    2020年11月16日16:23:51
    王伟

--- vue.config.js
+++ vue.config.js
@@ -84,7 +84,8 @@ const vueConfig = {
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:19013',
+        // target: 'http://127.0.0.1:19013',
+        target: 'https://mda.adsgreat.cn',
         ws: false,
         changeOrigin: true
       }
